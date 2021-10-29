b1a0f77a12a7f75cc44b9398db6784d1193d130f
Oleh Moroz <olehmoroz8@gmail.com>
2021-10-29 18:15:12 +0300

    fix testimonail

--- scripts/main.js
+++ scripts/main.js
@@ -5,17 +5,31 @@ document.addEventListener('DOMContentLoaded', () => {
         brandSubTitle = document.querySelector('.brands-description'),
         brandsLogo = document.querySelector('.brands-logo_wrap'),
         testimonialWrap = document.querySelector('.testimonial-row'),
-        testimonialWrapHeight = testimonialWrap.offsetHeight,
-        testimonialWrapHeightSmall = testimonialWrapHeight / 4,
+        testimonialItem = document.querySelectorAll('.testimonial-item'),
+        testimonialWrapHeight = (maxHeight() / testimonialItem.length) / 2.5,
+        testimonialWrapHeightSmall = testimonialWrapHeight / 3.5,
         buttonShowAll = document.createElement('button');
 
         buttonShowAll.classList.add('button-show-all');
         buttonShowAll.innerHTML = 'show all';
         testimonialWrap.appendChild(buttonShowAll);
+        testimonialWrap.style.height = `${testimonialWrapHeight}px`;
         buttonShowAll.style.display = 'none';
 
         let resizeValidate = false;
-        const btnShow = document.querySelector('.button-show-all');
+        const btnShow = document.querySelector('.button-show-all'); 
+
+        function maxHeight() {
+            let maxheight = 0;
+
+            testimonialItem.forEach(element => {
+                for (let i = 0; i < testimonialItem.length; i++) {
+                    maxheight = maxheight + element.offsetHeight;
+                }
+            });
+
+            return maxheight;
+        }
 
     function hiddenTestimonialMobile() {
             brandsLogo.appendChild(brandSubTitle);
@@ -32,25 +46,45 @@ document.addEventListener('DOMContentLoaded', () => {
         if (!resizeValidate && windowWidth < 753) {
             hiddenTestimonialMobile();
         } else {
-            testimonialWrap.style.height = 'auto';
+            if (windowWidth < 1070) {
+                const testimonialWrapHeightNew = (maxHeight() / testimonialItem.length) / 2.5;
+                      testimonialWrap.style.height = `${testimonialWrapHeightNew}px`;
+            } else {
+                const testimonialWrapHeightNew = (maxHeight() / testimonialItem.length) / 1.5;
+                testimonialWrap.style.height = `${testimonialWrapHeightNew}px`;
+            }
+
             buttonShowAll.style.display = 'none';
+            document.querySelector('.brands-content').appendChild(brandSubTitle);
         }
+
     });
+
+    if (windowWidthMain < 1070) {
+        const testimonialWrapHeightNew = (maxHeight() / testimonialItem.length) / 2.5;
+        testimonialWrap.style.height = `${testimonialWrapHeightNew}px`;
+    }
 
     if (windowWidthMain < 753) {
         hiddenTestimonialMobile();
+        const testimonialWrapHeightNew = (maxHeight() / testimonialItem.length) / 2.5;
+        testimonialWrap.style.height = `${testimonialWrapHeightNew}px`;
     }
 
     btnShow.addEventListener('click', () => {
+        const testimonialWrapHeightNew = (maxHeight() / testimonialItem.length) / 2.5,
+            testimonialWrapHeightSmallNew = testimonialWrapHeightNew;
 
-        if (testimonialWrap.style.height == `${testimonialWrapHeightSmall}px`) {
-            testimonialWrap.style.height = `${testimonialWrapHeight}px`;
+
+        if (!btnShow.classList.contains('button-show-all-active')) {
+            testimonialWrap.style.height = `${testimonialWrapHeightNew}px`;
 
             btnShow.classList.add('button-show-all-active');
             buttonShowAll.innerHTML = 'hide';
         } else {
-            testimonialWrap.style.height = `${testimonialWrapHeightSmall}px`;
+            testimonialWrap.style.height = `${testimonialWrapHeightSmallNew}px`;
             btnShow.classList.remove('button-show-all-active');
+            buttonShowAll.innerHTML = 'show all';
         }
     });
 
